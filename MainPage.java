00e317b8cbe7e9d95d1758c694819157ab0b0623
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-15 16:14:59 +0900

    Create MainPage.java

---
+++ MainPage.java
@@ -0,0 +1,14 @@
+package com.example.corona19app;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.corona_main);
+    }
+}
