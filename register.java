5cde3b341b58f0482508b1995feadc70164b9684
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-15 16:15:25 +0900

    Create register.java

---
+++ register.java
@@ -0,0 +1,13 @@
+package com.example.corona19app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class register extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
