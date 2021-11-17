8606046378f1bdddea0a06ab6bd927ac9893c4c0
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-17 18:08:53 +0900

    Create coronastate.java

---
+++ java/coronastate.java
@@ -0,0 +1,11 @@
+package com.example.corona19app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class corona_state extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.corona_state);
+    }
