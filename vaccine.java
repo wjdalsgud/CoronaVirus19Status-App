a358f525ae95697444a8ca089b77357c64a7c299
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-17 18:10:12 +0900

    Create vaccine.java

---
+++ vaccine.java
@@ -0,0 +1,14 @@
+package com.example.corona19app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class vaccine extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vaccine);
+    }
+}
