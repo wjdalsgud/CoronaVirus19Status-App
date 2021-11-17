1e21cfcae414b629a70b9cd3df917a69c709fb0d
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-17 18:08:21 +0900

    Update MainPage.java

--- java/MainPage.java
+++ java/MainPage.java
@@ -2,7 +2,10 @@ package com.example.corona19app;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPage extends Activity {
 
@@ -10,5 +13,31 @@ public class MainPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.corona_main);
+
+        Button loginBt4 = (Button)findViewById(R.id.socialbutton);
+        loginBt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), social_distancing.class);
+                startActivity(intent);
+            }
+        });
+
+        Button vaccinebutton = (Button)findViewById(R.id.vaccinebutton);
+        vaccinebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), vaccine.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button4 = (Button)findViewById(R.id.button5);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), corona_state.class);
+                startActivity(intent);
+            }
+        });
     }
-}
