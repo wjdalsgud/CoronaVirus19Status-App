8bd4feaac361b99b6921c8bd7fa2e2b7a7559bf2
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-22 16:14:33 +0900

    Update coronastate.java

--- java/coronastate.java
+++ java/coronastate.java
@@ -1,11 +1,25 @@
 package com.example.corona19app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class corona_state extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.corona_state);
+
+        Button bt1= (Button)findViewById(R.id.foreign_button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), foreign_corona_status.class);
+                startActivity(intent);
+            }
+        });
+
     }
+}
