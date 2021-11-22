2252ccc73bf4e74a794c7d49c964488412a6e8eb
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-22 16:17:55 +0900

    Update vaccine.java

--- java/vaccine.java
+++ java/vaccine.java
@@ -1,8 +1,10 @@
 package com.example.corona19app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class vaccine extends Activity {
@@ -10,5 +12,24 @@ public class vaccine extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vaccine);
+
+
+        Button bt3 = (Button)findViewById(R.id.button2);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), residual_vaccine.class);
+                startActivity(intent);
+            }
+        });
+        Button bt2= (Button)findViewById(R.id.button);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), vaccine_type.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
