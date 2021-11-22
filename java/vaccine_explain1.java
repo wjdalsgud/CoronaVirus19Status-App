0fabdf0ae8620ac8d9517ad322e0713e757959f0
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-22 16:18:44 +0900

    Create vaccine_explain1.java

---
+++ java/vaccine_explain1.java
@@ -0,0 +1,13 @@
+package com.example.corona19app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class vaccine_explain1 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vaccine_explain1);
+    }
+}
