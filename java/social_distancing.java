7bd8241f2cb640f4493ce4a47aab7af34d53f507
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-22 16:17:23 +0900

    Create social_distancing.java

---
+++ java/social_distancing.java
@@ -0,0 +1,13 @@
+package com.example.corona19app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class social_distancing extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.social_distancing);
+    }
+}
