1827f999f59c141bfb4db401353058fbe81c180b
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-11-22 16:16:30 +0900

    Create residual_vaccine.java

---
+++ java/residual_vaccine.java
@@ -0,0 +1,13 @@
+package com.example.corona19app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class residual_vaccine extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.residual_vaccine);
+    }
+}
