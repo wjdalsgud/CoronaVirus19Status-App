ca036138e23efab20acec718345d365345f2d14f
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-05 20:14:23 +0900

    Create UserAccount.java

---
+++ java/UserAccount.java
@@ -0,0 +1,38 @@
+/**
+* 사용자 계정 정보 모델 클래스
+ */
+public class UserAccount {
+
+    private String idToken; // FireBase Uid(고유 토근정보)
+    private String emailID; // 이메일 아이디
+    private String password; // 비밀번호
+
+    public UserAccount() {
+
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
