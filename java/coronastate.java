1d6a01357095305b44866a0558deee0d94a87178
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-05 19:53:32 +0900

    Update coronastate.java

--- java/coronastate.java
+++ java/coronastate.java
@@ -1,10 +1,11 @@
-package com.example.corona19app;
+package com.example.coronastatusapp;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class corona_state extends Activity {
     @Override
