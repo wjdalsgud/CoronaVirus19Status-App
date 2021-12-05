f8ca84e12336de768fed261425af80d3e7cfd915
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-05 19:52:44 +0900

    Update Search_ID_PW.java

--- java/Search_ID_PW.java
+++ java/Search_ID_PW.java
@@ -1,4 +1,4 @@
-package com.example.corona19app;
+package com.example.coronastatusapp;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -7,6 +7,6 @@ public class Search_ID_PW extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_id_and_pw);
+        setContentView(R.layout.search_id_pw);
     }
 }
