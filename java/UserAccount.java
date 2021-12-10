7b503c9bc0281a7b60e1006a41c21b19a64b5ccd
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-10 13:06:22 +0900

    Update UserAccount.java

--- java/UserAccount.java
+++ java/UserAccount.java
@@ -1,3 +1,7 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+
 /**
 * 사용자 계정 정보 모델 클래스
  */
@@ -6,7 +10,7 @@ public class UserAccount {
     private String idToken; // FireBase Uid(고유 토근정보)
     private String emailID; // 이메일 아이디
     private String password; // 비밀번호
-
+    private String name;
     public UserAccount() {
 
     }
@@ -35,4 +39,7 @@ public class UserAccount {
         this.password = password;
     }
 
+    public String getName() {return name;}
 
+    public void setName(String name) { this.name = name; }
+}
