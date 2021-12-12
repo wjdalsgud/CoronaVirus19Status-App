d8021e053a1101b41c30bf0f668ee8f33cfe6259
jojinseok <82083624+jojinseok@users.noreply.github.com>
2021-12-12 20:19:23 +0900

    Update Gyeongsangbuk.java

--- city/Gyeongsangbuk.java
+++ city/Gyeongsangbuk.java
@@ -3,7 +3,7 @@ package com.example.coronastatusapp;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Gyeongsangnam extends Activity {
+public class Gyeongsangbuk extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
