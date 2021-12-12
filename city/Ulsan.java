0e5a709c9f0f8703af2ba8409f0fc53abbad2e9d
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 20:02:22 +0900

    Create Ulsan.java

---
+++ city/Ulsan.java
@@ -0,0 +1,13 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ulsan extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ulsan);
+    }
+}
+
