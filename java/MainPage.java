72fb9ac413a3070a171207dc00496887d1eb1d42
jjy1116a <jjy1116a@gmail.com>
2021-12-12 12:10:01 +0900

    Update MainPage.java

--- java/MainPage.java
+++ java/MainPage.java
@@ -1,14 +1,24 @@
-package com.example.coronastatusapp;
+package com.example.coronavirus19status_app;
 
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class MainPage extends Activity {
+
+    private long backKeyPressedTime = 0;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +27,8 @@ public class MainPage extends Activity {
 
 
 
-        Button loginBt4 = (Button) findViewById(R.id.social_distancing_button);
-        loginBt4.setOnClickListener(new View.OnClickListener() {
+        Button social_distancing = (Button) findViewById(R.id.social_distancing_button);
+        social_distancing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), social_distancing.class);
@@ -26,8 +36,8 @@ public class MainPage extends Activity {
             }
         });
 
-        Button vaccinebutton1 = (Button) findViewById(R.id.vaccine_button);
-        vaccinebutton1.setOnClickListener(new View.OnClickListener() {
+        Button vaccine = (Button) findViewById(R.id.vaccine_button);
+        vaccine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), vaccine.class);
@@ -35,13 +45,59 @@ public class MainPage extends Activity {
             }
         });
 
-        Button button4 = (Button) findViewById(R.id.corona_state_button);
-        button4.setOnClickListener(new View.OnClickListener() {
+        Button corona_state = (Button) findViewById(R.id.corona_state_button);
+        corona_state.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), corona_state.class);
                 startActivity(intent);
             }
         });
+
+        BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()) {
+                    case R.id.first_Tab:
+                        onBackPressed();
+                        return true;
+                    case R.id.second_Tab:
+                        return true;
+                    case R.id.third_Tab:
+                        intent = new Intent(getApplicationContext(), social_distancing.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.fourth_Tab:
+                        intent = new Intent(getApplicationContext(), vaccine.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.fifth_Tab:
+                        intent = new Intent(getApplicationContext(), corona_state.class);
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            finish();
+            toast.cancel();
+            toast = Toast.makeText(this,"이용해 주셔서 감사합니다.",Toast.LENGTH_LONG);
+            toast.show();
+            moveTaskToBack(true);
+            finishAndRemoveTask();
+            System.exit(0);
+        }
     }
 }
