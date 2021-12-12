e0873de7980d44f4f8981e80ca312e2c66cce977
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 20:01:18 +0900

    Create Jeollanam.java

---
+++ city/Jeollanam.java
@@ -0,0 +1,12 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Jeollanam extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jeollanam);
+    }
+}
