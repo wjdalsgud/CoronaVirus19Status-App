709a7013f1fd33a880be056e06d108e6a52f4102
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 20:01:55 +0900

    Create Seoul.java

---
+++ city/Seoul.java
@@ -0,0 +1,16 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Seoul extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seoul);
+    }
+}
