c32aefdaa4e476d036e26fd1180c079dc787ec21
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 20:00:40 +0900

    Create Incheon.java

---
+++ city/Incheon.java
@@ -0,0 +1,13 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Incheon extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.incheon);
+    }
+}
+
