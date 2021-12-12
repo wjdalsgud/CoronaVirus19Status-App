8524e31f4bb228f7a7d31a5ddaf66c3cccfac807
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 20:00:18 +0900

    Create Gyeongsangnam.java

---
+++ city/Gyeongsangnam.java
@@ -0,0 +1,12 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gyeongsangnam extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gyeongsangnam);
+    }
+}
