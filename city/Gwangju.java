b6b7ebc5aa8b6615ec917fc831093998b4083372
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 19:59:33 +0900

    Create Gwangju.java

---
+++ city/Gwangju.java
@@ -0,0 +1,14 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gwangju extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gwangju);
+    }
+}
+
+
