038430ee87223001f2397e71663d90d3683e830d
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 19:57:55 +0900

    Create Busan.java

---
+++ city/Busan.java
@@ -0,0 +1,12 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Busan extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.busan);
+    }
+}
