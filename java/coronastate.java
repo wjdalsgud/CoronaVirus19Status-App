22d7b6f87363252e2638f2fd49e7f7a41578e45e
jjy1116a <jjy1116a@gmail.com>
2021-12-12 12:10:37 +0900

    Update coronastate.java

--- java/coronastate.java
+++ java/coronastate.java
@@ -1,10 +1,16 @@
-package com.example.coronastatusapp;
+package com.example.coronavirus19status_app;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class corona_state extends Activity {
@@ -13,12 +19,40 @@ public class corona_state extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.corona_state);
 
-        Button bt1= (Button)findViewById(R.id.foreign_button);
-        bt1.setOnClickListener(new View.OnClickListener() {
+        Button foreign_corona= (Button)findViewById(R.id.foreign_button);
+        foreign_corona.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), foreign_corona_status.class);
                 startActivity(intent);
+            }
+        });
+
+        BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+                switch (item.getItemId()) {
+                    case R.id.first_Tab:
+                        onBackPressed();
+                        return true;
+                    case R.id.second_Tab:
+                        intent = new Intent(getApplicationContext(), MainPage.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.third_Tab:
+                        intent = new Intent(getApplicationContext(), social_distancing.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.fourth_Tab:
+                        intent = new Intent(getApplicationContext(), vaccine.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.fifth_Tab:
+                        return true;
+                }
+                return false;
             }
         });
 
