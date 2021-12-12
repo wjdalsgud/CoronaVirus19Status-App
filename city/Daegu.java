63891ef4f8f9698b75179966617492e5698c8ed3
wjdalsgud <82083605+wjdalsgud@users.noreply.github.com>
2021-12-12 19:58:18 +0900

    Create Daegu.java

---
+++ city/Daegu.java
@@ -0,0 +1,13 @@
+package com.example.coronastatusapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Daegu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daegu);
+    }
+}
+
