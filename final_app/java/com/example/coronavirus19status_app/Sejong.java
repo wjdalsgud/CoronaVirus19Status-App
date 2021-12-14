7fa671478190af074fc069a8e208e628454f0e3d
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 21:38:18 +0900

    Update Sejong.java

--- final_app/java/com/example/coronavirus19status_app/Sejong.java
+++ final_app/java/com/example/coronavirus19status_app/Sejong.java
@@ -15,13 +15,13 @@ public class Sejong extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sejong);
+        setContentView(R.layout.sejong); //세종 화면 출력
 
         TextView sejongText = (TextView) findViewById(R.id.Sejong_API);
-        sejongText.setText(((MainActivity) MainActivity.mContext).getSejong());
+        sejongText.setText(((MainActivity) MainActivity.mContext).getSejong()); //MainActivty에서 파싱한 코로나 API 정보값을 해당 텍스트에 출력
 
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {// 하단 메뉴 바 동작 기능
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
