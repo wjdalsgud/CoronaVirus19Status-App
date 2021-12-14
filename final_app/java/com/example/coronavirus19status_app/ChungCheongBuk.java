443ca5139c7757f89dd17a04f28c76abfd502b33
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 21:39:16 +0900

    Update ChungCheongBuk.java

--- final_app/java/com/example/coronavirus19status_app/ChungCheongBuk.java
+++ final_app/java/com/example/coronavirus19status_app/ChungCheongBuk.java
@@ -15,13 +15,13 @@ public class ChungCheongBuk extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chungcheongbuk);
+        setContentView(R.layout.chungcheongbuk); //충북 화면 출력
 
         TextView chungcheongbukText = (TextView) findViewById(R.id.Chungcheongbuk_API);
-        chungcheongbukText.setText(((MainActivity) MainActivity.mContext).getChungcheongbuk());
+        chungcheongbukText.setText(((MainActivity) MainActivity.mContext).getChungcheongbuk()); //MainActivty에서 파싱한 코로나 API 정보값을 해당 텍스트에 출력
 
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { // 하단 메뉴 바 동작 기능
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
