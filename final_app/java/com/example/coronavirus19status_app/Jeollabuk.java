1d80e7ed3be86859f042e6bd4d81f4103b851e69
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 21:44:21 +0900

    Update Jeollabuk.java

--- final_app/java/com/example/coronavirus19status_app/Jeollabuk.java
+++ final_app/java/com/example/coronavirus19status_app/Jeollabuk.java
@@ -15,13 +15,13 @@ public class Jeollabuk extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.jeollabuk);
+        setContentView(R.layout.jeollabuk); //전라북도 화면 출력
 
         TextView jeollabukText = (TextView) findViewById(R.id.Jeollabuk_API);
-        jeollabukText.setText(((MainActivity) MainActivity.mContext).getJeollabuk());
+        jeollabukText.setText(((MainActivity) MainActivity.mContext).getJeollabuk()); //MainActivty에서 파싱한 코로나 API 정보값을 해당 텍스트에 출력
 
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { // 하단 메뉴 바 동작 기능
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
