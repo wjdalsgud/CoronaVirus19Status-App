6097665b0ce0dee4b2516c0b85da45126c31a17d
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 21:41:55 +0900

    Update Gyeonggi.java

--- final_app/java/com/example/coronavirus19status_app/Gyeonggi.java
+++ final_app/java/com/example/coronavirus19status_app/Gyeonggi.java
@@ -15,13 +15,13 @@ public class Gyeonggi extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gyeonggi);
+        setContentView(R.layout.gyeonggi); //경기도 화면 출력
 
         TextView gyeonggiText = (TextView) findViewById(R.id.Gyeonggi_API);
-        gyeonggiText.setText(((MainActivity) MainActivity.mContext).getGyeonggi());
+        gyeonggiText.setText(((MainActivity) MainActivity.mContext).getGyeonggi()); //MainActivty에서 파싱한 코로나 API 정보값을 해당 텍스트에 출력
 
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { // 하단 메뉴 바 동작 기능
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
