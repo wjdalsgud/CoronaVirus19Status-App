f0ea3709725c998255605ff0991fce481a7f0cd0
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 21:37:06 +0900

    Update Ulsan.java

--- final_app/java/com/example/coronavirus19status_app/Ulsan.java
+++ final_app/java/com/example/coronavirus19status_app/Ulsan.java
@@ -15,13 +15,13 @@ public class Ulsan extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ulsan);
+        setContentView(R.layout.ulsan); //울산 화면 출력
 
         TextView ulsanText = (TextView) findViewById(R.id.Ulsan_API);
-        ulsanText.setText(((MainActivity) MainActivity.mContext).getUlsan());
+        ulsanText.setText(((MainActivity) MainActivity.mContext).getUlsan()); //MainActivty에서 파싱한 코로나 API 정보값을 해당 텍스트에 출력
 
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {// 하단 메뉴 바 동작 기능
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
