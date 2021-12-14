8c13820ac3dffb98010203b31cdbf859ad6b2b62
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 18:37:36 +0900

    Update vaccine_type.java

--- final_app/java/com/example/coronavirus19status_app/vaccine_type.java
+++ final_app/java/com/example/coronavirus19status_app/vaccine_type.java
@@ -18,7 +18,7 @@ public class vaccine_type extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vaccine_type);
-
+// 화이자 버튼 클릭시 화이자 화면으로 이동
         Button pfizer= (Button)findViewById(R.id.Pfizer_Button);
         pfizer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +27,7 @@ public class vaccine_type extends Activity {
                 startActivity(intent);
             }
         });
+        //모더나 버튼 클릭시 모더나 화면으로 이동
         Button moderna= (Button)findViewById(R.id.Moderna_button);
         moderna.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +36,7 @@ public class vaccine_type extends Activity {
                 startActivity(intent);
             }
         });
-
+//얀센 버튼 클릭시 얀센 화면으로 이동
         Button janssen= (Button)findViewById(R.id.Janssen_button);
         janssen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +45,7 @@ public class vaccine_type extends Activity {
                 startActivity(intent);
             }
         });
-
+//아스트라제네카 버튼 클릭 시 아스트라제네카 화면으로 이동
         Button astra= (Button)findViewById(R.id.Astrazeneca_button);
         astra.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +54,7 @@ public class vaccine_type extends Activity {
                 startActivity(intent);
             }
         });
-
+//하단 메뉴바 동작 기능
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
         bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
