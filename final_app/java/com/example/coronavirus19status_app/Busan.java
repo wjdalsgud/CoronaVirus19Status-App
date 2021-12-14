7519e497093b38e9b574987174bff74c71dcd2fe
JoJinSeok <82083624+jojinseok@users.noreply.github.com>
2021-12-14 21:38:49 +0900

    Update Busan.java

--- final_app/java/com/example/coronavirus19status_app/Busan.java
+++ final_app/java/com/example/coronavirus19status_app/Busan.java
@@ -15,13 +15,13 @@ public class Busan extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.busan);
+        setContentView(R.layout.busan); //부산 화면 출력
 
         TextView busanText = (TextView) findViewById(R.id.Busan_API);
-        busanText.setText(((MainActivity) MainActivity.mContext).getBusan());
+        busanText.setText(((MainActivity) MainActivity.mContext).getBusan()); //MainActivty에서 파싱한 코로나 API 정보값을 해당 텍스트에 출력
 
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        bottom_menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { // 하단 메뉴 바 동작 기능
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent;
